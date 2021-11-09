82d1ab3a3e29df39344f7cd6578e7ed871d1beb1
joao.calixto <joao.calixto@basis.com.br>
2021-11-09 18:59:33 -0300

    commit de teste

---
+++ taskhook/src/main/java/br/gov/basis/taskmanager/taskhook/web/rest/WebHookResources.java
@@ -0,0 +1,23 @@
+package br.gov.basis.taskmanager.taskhook.web.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/webhook")
+@RequiredArgsConstructor
+public class WebHookResources {
+
+    @PostMapping
+    public ResponseEntity<Void> webhookListener(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(object);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
