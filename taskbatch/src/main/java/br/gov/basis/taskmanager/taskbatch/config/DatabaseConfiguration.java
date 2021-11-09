1c3ce4c44d16366d6471f3dd94e937d1db96dcc2
joao.calixto <joao.calixto@basis.com.br>
2021-11-08 21:33:58 -0300

    SETUP - configuração dos microsserviços concluída

--- taskbatch/src/main/java/br/gov/basis/taskmanager/taskbatch/config/DatabaseConfiguration.java
+++ taskbatch/src/main/java/br/gov/basis/taskmanager/taskbatch/config/DatabaseConfiguration.java
@@ -1,13 +1,8 @@
 package br.gov.basis.taskmanager.taskbatch.config;
 
-import io.github.jhipster.config.JHipsterConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -15,7 +10,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableJpaRepositories("br.gov.basis.taskmanager.taskbatch.repository")
 @EnableTransactionManagement
-@EnableElasticsearchRepositories("br.gov.basis.taskmanager.taskbatch.repository.search")
 public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
